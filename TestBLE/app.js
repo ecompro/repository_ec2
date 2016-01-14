6cab8b4f2d38591c5b722bc121d28af728bf00f8
Albert Garvett <ecomprotech@gmail.com>
2016-01-14 17:33:53 +0000

    Changes from Views service

--- TestBLE/app.js
+++ TestBLE/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-(function() {
+(function () {
     var app = {
         data: {}
     };
 
-    var bootstrap = function() {
-        $(function() {
+    var bootstrap = function () {
+        $(function () {
             app.mobileApp = new kendo.mobile.Application(document.body, {
                 transition: 'slide',
                 skin: 'flat',
@@ -16,19 +16,19 @@
     };
 
     if (window.cordova) {
-        document.addEventListener('deviceready', function() {
+        document.addEventListener('deviceready', function () {
             if (navigator && navigator.splashscreen) {
                 navigator.splashscreen.hide();
             }
 
             var element = document.getElementById('appDrawer');
-            if (typeof(element) != 'undefined' && element !== null) {
+            if (typeof (element) != 'undefined' && element !== null) {
                 if (window.navigator.msPointerEnabled) {
-                    $('#navigation-container').on('MSPointerDown', 'a', function(event) {
+                    $('#navigation-container').on('MSPointerDown', 'a', function (event) {
                         app.keepActiveState($(this));
                     });
                 } else {
-                    $('#navigation-container').on('touchstart', 'a', function(event) {
+                    $('#navigation-container').on('touchstart', 'a', function (event) {
                         app.keepActiveState($(this));
                     });
                 }
@@ -48,7 +48,7 @@
 
     window.app = app;
 
-    app.isOnline = function() {
+    app.isOnline = function () {
         if (!navigator || !navigator.connection) {
             return true;
         } else {
@@ -60,12 +60,90 @@
 // START_CUSTOM_CODE_kendoUiMobileApp
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
-(function() {
+(function () {
 
-    ble.scan([], 5, function(device) {
-        console.log(JSON.stringify(device));
-    }, failure);
+    //    ble.scan([], 5, function (device) {
+    //        console.log(JSON.stringify(device));
+    //    }, failure);
 
+
+    My.Scan = function () {
+        exports.defineAutoTests = function () {
+
+            describe('BLE object', function () {
+                it("ble should exist", function () {
+                    expect(ble).toBeDefined();
+                });
+
+                it("should contain a startScan function", function () {
+                    expect(typeof ble.startScan).toBeDefined();
+                    expect(typeof ble.startScan).toBe("function");
+                });
+            });
+
+        };
+
+        exports.defineManualTests = function (contentEl, createActionButton) {
+
+            createActionButton('Is Bluetooth Enabled?', function () {
+
+                ble.isEnabled(
+                    function () {
+                        console.log("Bluetooth is enabled");
+                    },
+                    function () {
+                        console.log("Bluetooth is *not* enabled");
+                    }
+                );
+            });
+
+
+            if (cordova.platformId !== 'ios') {
+
+                // not supported on iOS
+                createActionButton('Show Bluetooth Settings', function () {
+                    ble.showBluetoothSettings();
+                });
+
+                // not supported on iOS
+                createActionButton('Enable Bluetooth', function () {
+
+                    ble.enable(
+                        function () {
+                            console.log("Bluetooth is enabled");
+                        },
+                        function () {
+                            console.log("The user did *not* enable Bluetooth");
+                        }
+                    );
+                });
+
+            }
+
+            createActionButton('Scan', function () {
+
+                var scanSeconds = 5;
+                console.log("Scanning for BLE peripherals for " + scanSeconds + " seconds.");
+                ble.startScan([], function (device) {
+                    console.log(JSON.stringify(device));
+                }, function (reason) {
+                    console.log("BLE Scan failed " + reason);
+                });
+
+                setTimeout(ble.stopScan,
+                    scanSeconds * 1000,
+                    function () {
+                        console.log("Scan complete");
+                    },
+                    function () {
+                        console.log("stopScan failed");
+                    }
+                );
+
+            });
+
+        };
+    }
 }());
 
 // END_CUSTOM_CODE_kendoUiMobileApp
